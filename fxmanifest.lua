c0187b8a1277a434b1293a076c67419dd3966af2
JonarSonaer <haikara98@gmail.com>
2022-02-17 01:48:52 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+description 'SKRP-loading'
+version '1.1'
+author 'JonarSonar'
+
+files {
+	'ui.html',
+	'html/*',
+	'assets/*',
+}
+
+loadscreen {
+	'ui.html'
+}
